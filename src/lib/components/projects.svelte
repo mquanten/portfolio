2eca8ee5d656e59e017816ee053fb6605a815f0e
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-04 22:08:56 +0000

    added image

--- src/lib/components/projects.svelte
+++ src/lib/components/projects.svelte
@@ -2,10 +2,8 @@
   import ProjectCard from './project-card.svelte'
 </script>
 
-<div class="container flex-col items-start md:items-center md:mx-auto">
-  <h2
-    class="pl-4 mb-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 md:pl-12 md:text-4xl text-1xl"
-  >
+<div class="container flex-col items-start md:items-center">
+  <h2 class="pl-4 mb-4 title md:pl-12">
     Projects
   </h2>
   <div class="flex w-screen pl-4 ml-4 overflow-x-auto md:flex-wrap md:justify-center md:pl-12 slider max-w-96">
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,12 @@
-<h1>About</h1>
+<div class='container flex-col'>
+  <h1 class='title'>About</h1>
+    <img src='avatar.jpeg' alt='avatar' class='w-32 h-32 mt-4 rounded-full'>
+    <article class='max-w-lg prose'>
+      <p class='max-w-sm'>Hello again, as you probably already found out from my home page, my name is Mac and I am a frontend developer. I specialise in building sleek, fast and easy to maintain websites these sites can hook up to a CMS if you need to be able to control your data, I can even add more app like functionality such as user sign up and much more.</p>
+    </article>
+</div>
+<style>
+  p {
+    max-width: 90vw;
+  }
+</style>
