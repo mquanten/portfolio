f3463ffaa8f1a0184fc086f630aa1d4d39049008
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-04 17:16:40 +0000

    rearranged on desktop

--- src/lib/components/projects.svelte
+++ src/lib/components/projects.svelte
@@ -4,16 +4,16 @@
 
 <div class="container flex-col items-start">
   <h2
-    class="text-1xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 pl-4 mb-4"
+    class="text-1xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 pl-4 mb-4 md:pl-12 md:text-4xl"
   >
     Projects
   </h2>
-  <div class="slider w-screen flex md:flex-flow overflow-x-auto pl-4">
+  <div class="slider w-screen flex md:flex-flow overflow-x-auto pl-4 md:pl-12">
     <ProjectCard />
     <ProjectCard />
   </div>
   <button
-    class="primary ml-4 mt-4"
+    class="primary ml-4 mt-4 md:ml-12"
     on:click={() => (window.location.href = '/projects')}
   >
     View All
