ebed366f26133769e59166709cf8df8930adc3c6
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 23:09:40 +0000

    Update project-list.svelte

--- src/lib/components/project-list.svelte
+++ src/lib/components/project-list.svelte
@@ -2,7 +2,7 @@
   import ProjectCard from './project-card.svelte'
 </script>
 
-<div class="flex flex-col md:items-center">
+<div class="flex snap-x snap-mandatory flex-col md:items-center">
   <div class="flex w-screen py-8 pl-4 ml-4 overflow-x-auto pymd:py-24 md:flex-wrap md:justify-center md:pl-12 slider max-w-96">
     <ProjectCard />
     <ProjectCard />
