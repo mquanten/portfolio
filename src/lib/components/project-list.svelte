4ddd1e284a0968cc72cb309c54060fc2d9efbc78
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 12:45:53 +0000

    Update project-list.svelte

--- src/lib/components/project-list.svelte
+++ src/lib/components/project-list.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col md:items-center">
-  <div class="flex w-screen py-8 pl-4 ml-4 erflow-x-auto pymd:py-24 md:flex-wrap md:justify-center md:pl-12 slider max-w-96">
+  <div class="flex w-screen py-8 pl-4 ml-4 overflow-x-auto pymd:py-24 md:flex-wrap md:justify-center md:pl-12 slider max-w-96">
     <ProjectCard />
     <ProjectCard />
     <ProjectCard />
