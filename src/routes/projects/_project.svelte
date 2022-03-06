5755f2f08898986f060bd70f84741f58e6399497
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 22:40:02 +0000

    Update _project.svelte

--- src/routes/projects/_project.svelte
+++ src/routes/projects/_project.svelte
@@ -24,7 +24,7 @@
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
   </div>
   <section id='header' class='flex flex-col items-start justify-start p-4 mt-12'>
-    <img class='rounded-2xl' src={cover} alt={title}/>
+    <img class='rounded-2xl aspect-2' src={cover} alt={title}/>
     <h1 class='m-0'>{title}</h1>
     <span class='text-slate-500'>{date}</span>
   </section>
