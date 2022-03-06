968d740ac5fa39026c0ccb6f05a943c834bad313
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 22:34:26 +0000

    Update _blog.svelte

--- src/routes/blog/_blog.svelte
+++ src/routes/blog/_blog.svelte
@@ -19,16 +19,16 @@
   <meta property="og:article:author:gender" content="Male" />
   <meta property="og:article:tag" content={tags} />
 </svelte:head>
-<article class='relative flex flex-col items-center p-2 mx-auto prose max-w-prose'>
+<article class='relative flex flex-col items-center p-4 mx-auto prose max-w-prose'>
   <div class='absolute top-0 left-0 py-1 pl-3 cursor-pointer' on:click={() => window.location.href='/blog'}>
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
   </div>
-  <section id='header' class='flex flex-col items-start justify-start p-2 mt-12'>
+  <section id='header' class='flex flex-col items-start justify-start p-4 mt-12'>
     <img class='rounded-2xl' src={cover} alt={title}/>
     <h1 class='m-0'>{title}</h1>
     <span class='text-slate-500'>{date}</span>
   </section>
-  <section id='content' class="p-2">
+  <section id='content' class="p-4">
     <slot/>
     <p>Thanks for reading this post.</p>
     <div class='flex flex-col items-start space-y-2'>
