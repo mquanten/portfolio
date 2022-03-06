712fdb444386652aae8a1f522644a681eeac93dd
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 13:09:56 +0000

    Update post-item.svelte

--- src/lib/components/post-item.svelte
+++ src/lib/components/post-item.svelte
@@ -4,7 +4,7 @@
 >
   <div class="p-4">
     <h2 class="text-xl font-semibold">Title</h2>
-    <span class="text-xs text-slate-400">00.00.00</span>
+    <span class="text-xs text-slate-500">00.00.00</span>
     <p class="py-2 line-clamp-2">
       A snippet of text goes here Lorem ipsum dolor sit, amet consectetur
       adipisicing elit. Est, nemo.
