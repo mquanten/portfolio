3ef9002b35a4737437b69d2026171c0bfe3fabdb
Mac <5686378+mquanten@users.noreply.github.com>
2022-03-06 12:20:26 +0000

    fix: sizing of post-items

--- src/lib/components/post-item.svelte
+++ src/lib/components/post-item.svelte
@@ -1,7 +1,10 @@
-<a href="/" class="flex items-center w-5/6 my-4 shadow-sm lg:w-11/12 md:w-4/6">
+<a
+  href="/"
+  class="flex items-center rounded-2xl w-5/6 my-4 shadow-sm lg:w-3/4 md:w-4/6"
+>
   <div class="p-4">
     <h2 class="text-xl font-semibold">Title</h2>
-    <span class="text-xs">00.00.00</span>
+    <span class="text-xs text-slate-400">00.00.00</span>
     <p class="py-2 line-clamp-2">
       A snippet of text goes here Lorem ipsum dolor sit, amet consectetur
       adipisicing elit. Est, nemo.
