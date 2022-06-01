d68a9ec8a543c217b7e5780d565fa4fa7d0d3b59
Mac <mac@mail.com>
2022-06-01 15:37:33 +0100

    added abrasion api

---
+++ src/routes/projects/abrasion/index.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const posts = await fetch('/api/abrasion.json')
+    const allPosts = await posts.json()
+
+    return {
+      props: {
+        documents: allDocuments
+      }
+    }
+  }
+</script>
+
+<script>
+  import PostList from '$lib/components/post-list.svelte'
+  export let documents
+</script>
+
+<svelte:head>
+  <title>Abrasion | Mac</title>
+</svelte:head>
+<div class="container flex-col py-6">
+  <h1 class="mb-4 title">Blog</h1>
+  <PostList {documents} />
+</div>
+
