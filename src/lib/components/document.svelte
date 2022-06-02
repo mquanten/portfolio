15fd0f5da67d61bc34977654ed80981f00febb30
Mac <mac@mail.com>
2022-06-02 11:35:54 +0100

    fixed width of Documets

--- src/lib/components/document.svelte
+++ src/lib/components/document.svelte
@@ -1,11 +1,8 @@
 <script>
-  export let title, path
+  export let title, path;
 </script>
 
-<a
-  href={path}
-  class="flex items-center w-5/6 my-4 shadow-sm rounded-2xl lg:w-1/2 md:w-4/6"
->
+<a href={path} class="flex items-center my-4 shadow-sm rounded-2xl">
   <div class="p-4">
     <h2 class="text-xl font-semibold">{title}</h2>
     <button class="link">Read</button>
