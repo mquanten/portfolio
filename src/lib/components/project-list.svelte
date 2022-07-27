be294c56ff4f6686cf145fe98e1ec9de4eff0d48
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-27 20:46:28 +0100

    Update project-list.svelte

--- src/lib/components/project-list.svelte
+++ src/lib/components/project-list.svelte
@@ -7,12 +7,6 @@
   <div
     class="flex w-screen py-8 overflow-x-auto md:py-24 md:flex-wrap justify-center slider max-w-96"
   >
-    {#if projects.length < 1}
-      <div class="prose flex flex-col items-center">
-        <h2>There's nothing here yet.</h2>
-        <p>Keep your eyes on this space.</p>
-      </div>
-    {/if}
     {#each projects as project}
       <ProjectCard {...project.meta} path={project.path} />
     {/each}
