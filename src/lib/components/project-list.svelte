ea3594a4432140929d35c684d610b2f74c9fcb82
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-28 23:15:16 +0100

    decreased y padding on project-list

--- src/lib/components/project-list.svelte
+++ src/lib/components/project-list.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex snap-x snap-mandatory flex-col md:items-center">
   <div
-    class="flex w-screen py-8 overflow-x-auto md:flex-wrap justify-center slider max-w-96"
+    class="flex w-screen py-2 overflow-x-auto md:flex-wrap justify-center slider max-w-96"
   >
     {#each projects as project}
       <ProjectCard {...project.meta} path={project.path} />
