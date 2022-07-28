74fa0122b2ebbbf71a04c1ec35dd6a704370f553
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-28 22:48:23 +0100

    added y padding to blog title

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -19,7 +19,7 @@
 <svelte:head>
   <title>Blog | Mac</title>
 </svelte:head>
-<div class="container flex-col py-6">
+<div class="container flex-col py-8">
   <h1 class="mb-4 title">Blog</h1>
   <PostList {posts} />
 </div>
