2cc711bfe6683501cb45c1a0f7b6629aa496df63
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-28 23:19:23 +0100

    added overflow hidden to y axis in project-list

--- src/lib/components/project-list.svelte
+++ src/lib/components/project-list.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex snap-x snap-mandatory flex-col md:items-center">
   <div
-    class="flex w-screen overflow-x-auto md:flex-wrap pb-8 justify-center slider max-w-96"
+    class="flex w-screen overflow-x-auto md:flex-wrap pb-8 overflow-y-hidden justify-center slider max-w-96"
   >
     {#each projects as project}
       <ProjectCard {...project.meta} path={project.path} />
